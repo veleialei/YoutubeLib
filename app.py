73c6f3a87066bd976c2f7f395654f51610d5ac4c
Shuangjiao Hu <shuangjiaohu@shuangjiaos-mbp.lan>
2018-11-08 22:54:49 -0800

    add data base and for loop

--- app.py
+++ app.py
@@ -14,31 +14,54 @@ firebase = pyrebase.initialize_app(config)
 
 storage = firebase.storage()
 
+db = firebase.database()
 
+#db.child("id_record").push({"value":"0"})
 
+#db.child("names").child("name").update({"name":"shuangjiao"})
+
+# users = db.child("names").child("name").get()
+# print(users.key())
+# print(users.val())
+
+#users = db.child("names").child("name").remove()
+
+users = db.child("names").remove()
 app = Flask(__name__)
 
 i = 1
 
 @app.route('/', methods=['GET', 'POST'])
-
 def basic():
+    return render_template('index.html')
+    
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+    if request.method == 'POST':
+        if request.form['submit'] == 'add':
+            name = request.form['name']
+            db.child("todo").push(name)
+        elif request.form['submit'] == 'delete':
+            name = request.form['name']
+            db.child("todo").remove()
+    todo = db.child("todo").get()
+    to = todo.val()
+    if to == None:
+         to = {}
+    return render_template('todo.html', t=to.values())
+
+@app.route('/uploads')
+def uploads():
     global i
     if request.method=='POST':
         upload = request.files['upload']
         storage.child("music/test"+str(i)+".mp3").put(upload)
         i+=1
-        return redirect(url_for('uploads'))
-    return render_template('index.html')
-
-@app.route('/uploads')
-def uploads():
-    global i
-    if True:
-        links = storage.child('music/test' + str(i) + '.mp3').get_url(None)
-        print (links)
-        return render_template('upload.html', l = links)
-    return render_template('upload.html')
+    links = []
+    for id in range(1,3):
+        links.append(storage.child('music/test' + str(id) + '.mp3').get_url(None))
+    return render_template('upload.html', l = links)
 
 if __name__ == '__main__':
     app.run(debug=True)

29ec334cb7eb35d2cfcf63e60e057542f71d3248
veleialei <veleialei@gmail.com>
2018-11-17 16:06:36 -0800

    change structure

--- app/routes.py
+++ app/routes.py
@@ -1,43 +1,20 @@
+from app import app
+from flask import render_template, request
 import pyrebase
-from flask import *
+from config import Config
 
-config = {
-    "apiKey": "AIzaSyCK1JLNSyyAn4PQ94M_fWGLEBzm02NeMIk",
-    "authDomain": "sideproject-a7c5c.firebaseapp.com",
-    "databaseURL": "https://sideproject-a7c5c.firebaseio.com",
-    "projectId": "sideproject-a7c5c",
-    "storageBucket": "sideproject-a7c5c.appspot.com",
-    "messagingSenderId": "72493675211"
-}
-
-firebase = pyrebase.initialize_app(config)
-
+firebase = pyrebase.initialize_app(Config)
 storage = firebase.storage()
-
 db = firebase.database()
 
-#db.child("id_record").push({"value":"0"})
-
-#db.child("names").child("name").update({"name":"shuangjiao"})
-
-# users = db.child("names").child("name").get()
-# print(users.key())
-# print(users.val())
-
-#users = db.child("names").child("name").remove()
-
 users = db.child("names").remove()
-app = Flask(__name__)
 
 i = 1
 
 @app.route('/', methods=['GET', 'POST'])
 def basic():
     return render_template('index.html')
-    
-#此处要改成接受 youtube link
-#然后接收了立刻 download到本地
-#然后传到 storage 里面
+
 @app.route('/todo', methods=['GET', 'POST'])
 def todo():
     if request.method == 'POST':
@@ -66,5 +43,3 @@ def uploads():
         links.append(storage.child('music/test' + str(id) + '.mp3').get_url(None))
     return render_template('upload.html', l = links)
 
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,8 @@
+Config = {
+    "apiKey": "AIzaSyCK1JLNSyyAn4PQ94M_fWGLEBzm02NeMIk",
+    "authDomain": "sideproject-a7c5c.firebaseapp.com",
+    "databaseURL": "https://sideproject-a7c5c.firebaseio.com",
+    "projectId": "sideproject-a7c5c",
+    "storageBucket": "sideproject-a7c5c.appspot.com",
+    "messagingSenderId": "72493675211"
+}
